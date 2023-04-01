c5e8320a7ad6e9a934f1083fbfab8651640a247f
manoloramon <manolo.manatlao@codeflexitservices.com>
2023-04-01 23:23:36 +0400

    Learn the Basic Types in Elixir

---
+++ BasicTypes.exs
@@ -0,0 +1,22 @@
+1
+# Integers
+IO.puts(1)
+IO.puts(0x1F)
+
+# Float
+IO.puts(1.0)
+
+# Boolean
+IO.puts(true)
+
+# atom / symbol
+IO.puts(:atom)
+
+# String
+IO.puts("elixir")
+
+# list
+IO.puts([1, 2, 3])
+
+# Tuple
+IO.puts({1, 2, 3})
