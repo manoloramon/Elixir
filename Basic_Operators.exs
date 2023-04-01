9eabd97efa19049fd1a5771fcb4880d9c65b86dd
manoloramon <manolo.manatlao@codeflexitservices.com>
2023-04-01 23:31:27 +0400

    Learning Basic Operators

---
+++ Basic_Operators.exs
@@ -0,0 +1,18 @@
+# Addition
+IO.puts(1 + 2)
+
+# Multiplication
+IO.puts(5 * 5)
+
+# Division
+IO.puts(10 / 2)
+IO.puts(div(10, 2))
+IO.puts(div(10, 2))
+
+# Remainder
+IO.puts(rem(10, 3))
+
+# Binary and Hex
+IO.puts(0b1010)
+IO.puts(0o777)
+IO.puts(0x1F)
